3804c91b066be56a936f09ec7f44810f32a4b7db
kuldeepchotaliya111 <kuldeep.tagline@gmail.com>
2021-01-29 10:52:01 +0530

    'admin_and_super_admin_role_assign'

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(name:'kevin',email:'superadmin@gmail.com',phone:'9875648561',gender:'male',state_id: '1', city_id: '1',password:'superadmin123',password_confirmation:'superadmin123')
+user1.add_role :superadmin
+user2 = User.create(name:'pradip',email:'admin@gmail.com',phone:'9875689898',gender:'male',state_id: '1', city_id:'2', password:'admin123',password_confirmation:'admin123')
+user2.add_role :admin
