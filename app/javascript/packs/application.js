b936235ddc3b39c45e1c7e5bc3ca273d22bb5b79
kuldeepchotaliya111 <kuldeep.tagline@gmail.com>
2021-02-02 16:26:11 +0530

    jquery_added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "bootstrap"
-import jquery from 'jquery';
-window.$ = window.jquery = jquery;
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
