905c3b5c14b202c06b4cc1da0975e40889f882e5
kuldeepchotaliya111 <77951893+kuldeepchotaliya111@users.noreply.github.com>
2021-02-04 10:58:43 +0530

    dependent_dropdown_add (#6)
    * dependent_dropdown_add
    
    * dependent_dropdown_using_ajax
    
    * jquery_added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,18 @@
 
 # This controller is help to render home page
 class HomeController < ApplicationController
-  def index; end
+  def index 
+    if params[:state].present?
+      @cities = State.find(params[:state]).cities
+    else
+      @cities = []
+    end
+    if request.xhr?
+      respond_to do |format|
+        format.json {
+          render json: {cities: @cities}
+        }
+      end
+    end
+  end
 end
